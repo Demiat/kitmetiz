058166e2caedf8b65efeddd092d4d7999e2e3d68
Дмитрий Тарасов <demiat@mail.ru>
2025-06-21 17:09:17 +0300

    add widget class for OrderingFilter

--- kitm/welcome/filters.py
+++ kitm/welcome/filters.py
@@ -2,16 +2,31 @@ import django_filters
 from core.models import Nomenclature
 from django.db.models import Avg
 from django import forms
+from django.forms.widgets import Select
+
+
+class CustomSelectWidget(Select):
+    """Переопределим стиль селекта."""
+
+    def __init__(self, attrs=None, *args, **kwargs):
+        super().__init__(attrs={'class': 'form-select form-select-sm me-2'})
 
 
 class NomenclatureFilter(django_filters.FilterSet):
+    """Фильтрует и сортирует номенклатуру."""
+
+    category = django_filters.CharFilter(
+        field_name='category__slug',
+        label=False,
+        widget=forms.HiddenInput(),
+    )
 
     find_nom = django_filters.CharFilter(
         field_name='name',
         lookup_expr='iregex',
         label=False,
         widget=forms.TextInput(attrs={
-            'class': 'form-control flex-grow-1 me-2',
+            'class': 'form-control flex-grow-1 w-100 me-2',
             'type': 'search',
             'placeholder': 'Поиск по номенклатуре',
             'aria-label': 'Search'
@@ -21,6 +36,7 @@ class NomenclatureFilter(django_filters.FilterSet):
     # Фильтр сортировки по цене
     sort_by_price = django_filters.OrderingFilter(
         fields=(('price', 'price'),),
+        widget=CustomSelectWidget,
         label=False,
         choices=[
             ('price', 'Возрастание цены'),
@@ -31,6 +47,7 @@ class NomenclatureFilter(django_filters.FilterSet):
     # Фильтр сортировки по рейтингу
     sort_by_rating = django_filters.OrderingFilter(
         fields=(('ratings__rating', 'rating'),),
+        widget=CustomSelectWidget,
         label=False,
         choices=[
             ('-ratings__rating', 'От высшего к низшему'),
@@ -41,6 +58,7 @@ class NomenclatureFilter(django_filters.FilterSet):
     # Фильтр сортировки по названию
     sort_by_name = django_filters.OrderingFilter(
         fields=(('name', 'name'),),
+        widget=CustomSelectWidget,
         label=False,
         choices=[
             ('name', 'От А до Я'),
@@ -56,7 +74,11 @@ class NomenclatureFilter(django_filters.FilterSet):
 
     @property
     def qs(self):
-        result = super().qs.annotate(avg_rating=Avg('ratings__rating'))
+        result = super().qs.annotate(
+            avg_rating=Avg('ratings__rating')
+        ).select_related(
+            'category'
+        )
         orderings = []
         if self.data.get('sort_by_price'):
             orderings.append(self.data['sort_by_price'])
