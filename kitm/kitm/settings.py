95946179fc2a6d78d29a07af27dec24e02f9647a
Дмитрий Тарасов <demiat@demiat-air.local>
2025-06-25 18:07:35 +0300

    fix settings: postrges or sqlite

--- kitm/kitm/settings.py
+++ kitm/kitm/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Dict, Union
 import os
 
 from dotenv import load_dotenv
@@ -68,16 +69,23 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'kitm.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': os.getenv('ENGINE_POSTGRES', 'django.db.backends.sqlite3'),
-        'NAME': os.getenv('POSTGRES_DB', BASE_DIR / 'db.sqlite3'),
-        'USER': os.getenv('POSTGRES_USER', 'django'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', ''),
-        'PORT': os.getenv('DB_PORT', 5432)
-    }
+DATABASES: Dict[str, Union[str, int, object]] = {}
+POSTGRES = {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': os.getenv('POSTGRES_DB', 'django'),
+    'USER': os.getenv('POSTGRES_USER', 'django'),
+    'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+    'HOST': os.getenv('DB_HOST', ''),
+    'PORT': os.getenv('DB_PORT', 5432)
 }
+SQLite = {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': BASE_DIR / 'db.sqlite3',
+}
+if os.getenv('USE_POSTGRES', 'False') == 'True':
+    DATABASES['default'] = POSTGRES
+else:
+    DATABASES['default'] = SQLite
 
 AUTH_PASSWORD_VALIDATORS = [
     {
